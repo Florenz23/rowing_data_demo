c65d795736b1005c98a63a0a0bfbdbd8e95e9e64
Floreny <floreny@IES-loaner01.local>
2017-10-21 19:03:17 +0200

    firebase addeded

--- Assets/getData.cs
+++ Assets/getData.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class getData : MonoBehaviour {
 
 	// Use this for initialization
+	public TextMesh txt;
 	void Start () {
-		
+		txt = gameObject.GetComponent<TextMesh>();
+		txt.text="Score : ";
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
